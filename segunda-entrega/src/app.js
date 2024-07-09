5bd2a1a009ae463437188b900c0e255e969cd30c
mpachecoy <ing.pachecoymartin@gmail.com>
2024-07-09 11:40:38 -0300

    Segunda entrega

---
+++ segunda-entrega/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import routes from "./routes/index.js";
+import { Server } from "socket.io";
+import handlebars from "express-handlebars";
+import viewsRoutes from "./routes/views.routes.js";
+import __dirname from "./dirname.js";
+
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine("handlebars", handlebars.engine()); 
+app.set("views", __dirname + "/views"); 
+app.set("view engine", "handlebars"); 
+app.use(express.static("public"));
+
+app.use("/api", routes);
+app.use("/", viewsRoutes);
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
+});
+
+// Configuración de socket
+
+export const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado");
+});
